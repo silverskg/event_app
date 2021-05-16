d5ff1f9c2695c399ca8c7134cf155c2389229713
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-16 13:24:54 +0900

    userモデル作成

---
+++ config/initializers/amniauth.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  if Rails.env.development? || Rails.env.test?
+    provider :github, "ebcff22c215d8b8f4775", "1b4b026175ca77339de3e1ae6dfead3b0e51746e"
+  else
+    provider :github,
+    Rails.application.credentials.github[:client_id],
+    Rails.application.credentials.github[:client_secret]
+  end
+end
