18abd878d4e335a620232ce3ca79c9cc5b00c9ab
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-16 13:58:47 +0900

    gitログイン　ルーティング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    binding.pry
-    user = User.find_or_create_form_auth_hash!(rquest.env["omniauth.auth"])
+    user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path, notice: "ログインしました"
   end
