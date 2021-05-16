80a4876376646150259b40ad243391de72fe132b
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-16 16:43:45 +0900

    編集ボタン実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,17 @@
 class Event < ApplicationRecord
   belongs_to :owner, class_name: "User"
-  
+
   validates :name, length: { maximum: 50}, presence: true
   validates :place, length: { maximum: 100}, presence: true
   validates :content, length: { maximum: 2000}, presence: true
   validates :start_at, presence: true
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
+
+  def created_by?(user)
+    return false unless user
+    owner_id = user.id
+  end
 
   private
 
@@ -17,4 +22,5 @@ class Event < ApplicationRecord
       errors.add(:start_at, "は終了時間よりも前に設定してください")
     end
   end
+
 end
