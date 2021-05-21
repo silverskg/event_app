ecfe1dd973c7ef08cd1d932c32ccef53899e06f3
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-21 15:01:45 +0900

    イベント参加用ビュー ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+
+  resources :events do
+    resources :tickets
+  end
 end
