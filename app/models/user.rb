d99a2d524ed4dcc3f4affdbde4c3126c07e1d7d1
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-25 14:53:22 +0900

    Userモデルの削除時のコールバックを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
-class User < ApplicationRecord
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+class User < ApplicationRecord 
+  before_destory :check_all_events_finished
+
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participathing_events, through: :tickets, source: :event
   
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
@@ -12,5 +15,20 @@ class User < ApplicationRecord
       user.name = nickname
       user.image_url = image_url
     end
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "公開中の未開催のイベントが存在します。"
+    end
+
+    if participathing_events.where(":now < end_at", now: now).exists?
+      errors[:bose] << "未開催の参加イベントが存在します。"
+    end
+
+    throw(:abort) unless errors.empty?
   end
 end
