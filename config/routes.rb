f4c21bf774ffc73e3f07ea11292f54210a8d20c2
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-25 14:20:32 +0900

    retirement/ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
 
+  resource :retirements, only: %i[new create]
+
   resources :events do
     resources :tickets
   end
