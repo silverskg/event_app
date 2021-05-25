21edefa17d1dace0d9a1ad625b3d30b41d78e9c2
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-25 13:56:25 +0900

    外部制約キーの設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :owner, class_name: "User"
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
 
   validates :name, length: { maximum: 50}, presence: true
   validates :place, length: { maximum: 100}, presence: true
