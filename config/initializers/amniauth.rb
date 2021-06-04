bafd86e6c08b700c4cd02b21141f9bebab8df926
silverskg <chokichoki_gm@i.softbank.jp>
2021-06-04 14:50:06 +0900

    devise/github設定情報の変更

--- config/initializers/amniauth.rb
+++ config/initializers/amniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider_ignores_state: true 
   
   if Rails.env.development? || Rails.env.test?
     provider :github, "ebcff22c215d8b8f4775", "1b4b026175ca77339de3e1ae6dfead3b0e51746e"
@@ -9,3 +8,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     Rails.application.credentials.github[:client_secret]
   end
 end
+## 5/29 19時22~
