e0bcdfa9834d4e1bd5321a20d0ddaba749c4301e
silverskg <chokichoki_gm@i.softbank.jp>
2021-07-27 17:40:07 +0900

    form部分テンプレート化

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,7 @@ class EventsController < ApplicationController
     if @event.save
       redirect_to @event, notice: "作成しました"
     else
+      flash.now[:alert] = "未入力欄があります"
       render 'new'
     end
   end
