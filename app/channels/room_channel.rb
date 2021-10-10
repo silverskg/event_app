063fc837e09a064c1931ad5194bdcc801571af20
silverskg <chokichoki_gm@i.softbank.jp>
2021-10-10 16:29:26 +0900

    jquery roomチャネルの作成

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,12 +1,16 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "room_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    # jsで実行されたspeakのmessageを受け取り、room_channelのreceivedにブロードキャストする
+
+    # ActionCable.server.broadcast 'room_channel', message: data['message']
+    Message.create! content: data['message']
   end
 end
---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,20 @@
+const chatChannel = consumer.subscriptions.create("RoomChannel",{
+
+  received: function(data) {
+    return alert(data['message']);
+  },
+
+  speak: function(message){
+    return this.perform('speak',{
+      message: message
+    });
+  }
+});
+
+$(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+  if (event.keyCode === 13) {
+    chatChannel.speak(event.target.value);
+    event.target.value = '';
+    return event.preventDefault();
+  }
+});
