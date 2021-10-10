6d3571e92fab9be40eac00e7bfb10bc5799f91fc
silverskg <chokichoki_gm@i.softbank.jp>
2021-10-10 15:32:06 +0900

    rooms/showに対応するviewファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     # sessions: "users/sessions"
   }
 
+  get 'rooms/show'
+
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
